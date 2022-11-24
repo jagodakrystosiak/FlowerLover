7ece5909c1f8af38239332cbcca15db776616ba1
Jagoda Krystosiak <jagoda.krystosiak@gmail.com>
2022-11-24 13:39:17 +0100

    Add Plants view

--- client/src/pages/Plants/Plants.js
+++ client/src/pages/Plants/Plants.js
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import PlantBox from "../../components/PlantBox/PlantBox";
 import PlantList from "../../components/PlantList/PlantList";
 import Searchbar from "../../components/Searchbar/Searchbar";
+import HttpClient from "../../services/HttpClient";
+import './../Posts/Posts.scss';
 
 const Plants = () => {
     const [plants, setPlants] = useState([]);
+    const [species, setSpecies] = useState([]);
+    const [groupOfPlants, setGroupOfPlants] = useState([]);
     const [filterOptions, setFilterOptions] = useState({
         plantsPerPage: 10,
         sortType: "",
@@ -13,26 +17,26 @@ const Plants = () => {
 
     useEffect(() => {
         getPlants();
+        getSpecies();
+        getGroupOfPlants();
     },[])
 
-    const getPlants = () => {
-        let plants = [];
-        for(let i=0; i<30; i++){
-            plants[i] = {
-                id: 1,
-                customaryName: "Monstera biała",
-                demands: "Optymalna temperatura do uprawy monstery to 20 stopni C, ale jest w stanie całkiem dobrze przystosować się do zakresu od 15 do 30 stopni. Wymagania monstery nie są duże. Wykazuje ona dużą tolerancję względem światła, znosi suche powietrze a nawet kurz-w zakładach produkcyjnych.",
-                description: "Unikatowa, poszukiwana i bardzo cenna odmiana modnej monstery dziurawej, która zachwyca imponującym ubarwieniem. Jej duże liście z charakterystycznymi wycięciami cechują się pięknym pstrym ubarwieniem. Czasem bezchlorofilowa, kremowa struktura tkanek liścia pojawia się jako nieregularny akcent na fragmentach blaszek liściowych, a czasem obejmuje całe liście lub nawet fragmenty pędów. ",
-                height: 100,
-                humidity: 3,
-                imageUrl: "https://images.unsplash.com/photo-1630565963157-28ed2c8d3edc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-                name: "Monstera deliciosa 'Thai Constellation'",
-                placement: "Roślina odnajdzie się również na stanowiskach lekko zacienionych, znacznie spowolni to jednak jej wzrost.",
-                groupOfPlantsId: 1,
-                speciesId: 1
-            };
-        }
+    const getPlants = async () => {
+        const { data } = await HttpClient().get("/plants/all");
+        console.log(data);
+        let plants = data;
         setPlants(plants);
+    }
+
+    const getSpecies = async () => {
+        const { data } = await HttpClient().get("/species/all");
+        console.log(data);
+        setSpecies(data);
+    }
+
+    const getGroupOfPlants = async () => {
+        const { data } = await HttpClient().get("/groups-of-plants/all");
+        setGroupOfPlants(data);
     }
 
     const handlePlantsPerPageChange = (event) => setFilterOptions(prev=>({...prev, plantsPerPage: event.target.value}));
@@ -45,19 +49,11 @@ const Plants = () => {
                 <h1>Wyszukaj według</h1>
                 <h2>Gatunki</h2>
                 <ul>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
+                    {species.map((spec) => <li>{spec.name}</li>)}
                 </ul>
                 <h2>Grupy</h2>
                 <ul>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
-                    <li>gatunek</li>
+                    {groupOfPlants.map((group) => <li>{group.name}</li>)}
                 </ul>
             </div>
             <div className="content__list">
