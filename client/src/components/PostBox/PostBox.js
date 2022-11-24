7ae0917560c6bcf8074d526e1529c184764303c8
Jagoda Krystosiak <jagoda.krystosiak@gmail.com>
2022-11-24 13:38:24 +0100

    Fix Post view

--- client/src/components/PostBox/PostBox.js
+++ client/src/components/PostBox/PostBox.js
@@ -3,14 +3,15 @@ import Button from "../Button/Button";
 import './PostBox.scss';
 
 function PostBox({ post }) {
+    post.createDate = new Date(post.createDate);
     let date = "";
     if (post.createDate.valueOf() > 0) {
         let diffrence = Date.now().valueOf() - post.createDate.valueOf();
-        if(diffrence < 0) {
+        if (diffrence < 0) {
             date = "";
         }
         else if (diffrence > 86400000) {
-            let month = post.createDate.getMonth()+1;
+            let month = post.createDate.getMonth() + 1;
             date = post.createDate.getDate() + "-" + month + "-" + post.createDate.getFullYear();
 
         }
@@ -21,22 +22,22 @@ function PostBox({ post }) {
             date = Math.floor(diffrence / 60000) + " min temu";
         }
     }
-    
 
-return (
-    <div className="postbox">
-        <div className="postbox__post">
-            <a href={"/post/" + post.id}><h2 className="postbox__title">{post.title}</h2></a>
-            <p className="postbox__content">{post.content.substring(0,150)} ... </p>
-            <div className="postbox__bottom">
-                <p className="postbox__author">autor: <a href="/">{post.username}</a> {date}</p>
-                <div>
-                    {post.categories?.map((category) => <Button className="btn--light btn--small">{category.name}</Button>)}
+
+    return (
+        <div className="postbox">
+            <div className="postbox__post">
+                <a href={"/post/" + post.id}><h2 className="postbox__title">{post.title}</h2></a>
+                <p className="postbox__content">{post.content.substring(0, 150)} ... </p>
+                <div className="postbox__bottom">
+                    <p className="postbox__author">autor: <a href="/">{post.username}</a> <span>{date}</span></p>
+                    <div>
+                        {post.categories?.map((category) => <Button className="btn--light btn--small">{category.name}</Button>)}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default PostBox;
